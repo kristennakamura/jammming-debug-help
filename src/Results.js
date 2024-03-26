d4993fc368eca0d071f3240da0a539854a6e8157
Kristen Nakamura <kristen.nakamura@gmail.com>
2024-03-26 14:33:58 -0400

    debugging

--- src/Results.js
+++ src/Results.js
@@ -2,15 +2,39 @@ import Track from './Track.js'
 import styles from './styles/Results.module.css'
 
 
-const Results = ({trackList}) =>{
+const Results = ({trackList, addSong, removeSong, setPlaylistTracksData, playlistTracksData}) =>{
 
+
+    const checkIfAdded = (id) => {
+        console.log(id);
+        // console.log(playlistTracksData)
+        console.log(playlistTracksData.some(track => track.accessKey === id));
+        return playlistTracksData.length > 0 ? !playlistTracksData.some(track => track.accessKey === id) : true;
+    };
     
     return(
         <div className = {styles.results}>
             <header>
                 <h1 className = {styles.resultsHeader}>Results</h1>
             </header>
-            <ul className = {styles.trackList}>{trackList}</ul>
+            <ul className = {styles.trackList}>
+                {
+                trackList.map(track => (
+                    <Track
+                        key={track.id}
+                        accessKey={track.id}
+                        song={track.name}
+                        artist={track.artist}
+                        album={track.album}
+                        albumCover={track.albumCover}
+                        addSong={addSong}
+                        removeSong={removeSong}
+                        setPlaylistTracksData={setPlaylistTracksData}
+                        isAdd={checkIfAdded(track.id)}
+                    />
+                ))
+                }
+            </ul>
             {/*<Track song = 'song1' artist = 'artist1' album = 'album1'/>
             <Track song = 'song2' artist = 'artist2' album = 'album2' />*/}
         </div>
--- src/ResultsSection.js
+++ src/ResultsSection.js
@@ -2,14 +2,14 @@ import Results from './Results.js';
 import styles from './styles/ResultsSection.module.css';
 import Playlist from './Playlist.js'
 
-const ResultsSection = ({trackList, playlistTracksData}) => {
+const ResultsSection = ({trackList, playlistTracksData, addSong, removeSong, setPlaylistTracksData}) => {
     
 
     
     return(
         <div className = {styles.right}>
-            <Results trackList = {trackList}/>
-            <Playlist playlistTracksData = {playlistTracksData}/>
+            <Results trackList = {trackList} addSong={addSong} setPlaylistTracksData={setPlaylistTracksData} playlistTracksData = {playlistTracksData} removeSong={removeSong}/>
+            <Playlist playlistTracksData = {playlistTracksData} />
         </div>
     );
 }
